43b536a99994b48c40490b1820e6cea856bb7371
XinYaanZyoy <0lovesenergy@engineer.com>
2019-08-11 23:20:25 +0530

    a new website for Xbooks

---
+++ docs/Assets/js/index.js
@@ -0,0 +1,90 @@
+function updateExplorer(des) {
+
+    document.getElementById('loadImg').style.display = 'block';
+    document.getElementById('Xdisplay').style.display = 'none';
+
+    let explorer = document.getElementById("explorer");
+    let virtual = document.getElementsByClassName("virtual");
+
+    while (virtual[0]) {
+        virtual[0].parentNode.removeChild(virtual[0]);
+    }
+    par = "notebooks/";
+    des.split("/").forEach((ele) => {
+        if (ele != "notebooks" && ele != "welcome.html" && ele != "index.html" && ele != "\\$REPO$" && ele != "") {
+            let node = document.createElement("LI");
+            node.setAttribute("class", "breadcrumb-item virtual");
+            node.style.cursor = "pointer";
+            let a = document.createElement("A");
+            if (!ele.includes(".html")) {
+                a.setAttribute("onclick", `document.getElementById('Xdisplay').contentWindow.location.replace("${par}/${ele}/index.html"); updateExplorer("${par}/${ele}/index.html")`);
+                par = `${par}/${ele}`
+
+                let obj = Array(document.getElementsByClassName("tools"))[0]
+                for (let key of Object.keys(obj)){
+                    obj[key].style.display = "none";
+                }
+
+            } else {
+                a.setAttribute("onclick", `document.getElementById('Xdisplay').contentWindow.location.replace("${par}/${ele}"); updateExplorer("${par}/${ele}")`);
+                par = `${par}/${ele.replace(".html", "")}`
+
+                let obj = Array(document.getElementsByClassName("tools"))[0]
+                for (let key of Object.keys(obj)){
+                    obj[key].style.display = "";
+                }
+
+            }
+            let textnode = document.createTextNode(ele.replace(".html", ""));
+            a.appendChild(textnode);
+            node.appendChild(a);
+            explorer.appendChild(node);
+        }
+    })
+}
+
+
+function updateExplorer_IFrame(des) {
+
+    window.parent.document.getElementById('loadImg').style.display = 'block';
+    window.parent.document.getElementById('Xdisplay').style.display = 'none';
+
+    let explorer = window.parent.document.getElementById("explorer");
+    let virtual = window.parent.document.getElementsByClassName("virtual");
+
+    while (virtual[0]) {
+        virtual[0].parentNode.removeChild(virtual[0]);
+    }
+    par = "notebooks/";
+    des.split("/").forEach((ele) => {
+        if (ele != "notebooks" && ele != "welcome.html" && ele != "index.html" && ele != "\\$REPO$" && ele != "") {
+            let node = window.parent.document.createElement("LI");
+            node.setAttribute("class", "breadcrumb-item virtual");
+            node.style.cursor = "pointer";
+            let a = window.parent.document.createElement("A");
+            if (!ele.includes(".html")) {
+                a.setAttribute("onclick", `document.getElementById('Xdisplay').contentWindow.location.replace("${par}/${ele}/index.html"); updateExplorer("${par}/${ele}/index.html")`);
+                par = `${par}/${ele}`
+
+                let obj = Array(window.parent.document.getElementsByClassName("tools"))[0]
+                for (let key of Object.keys(obj)){
+                    obj[key].style.display = "none";
+                }
+
+            } else {
+                a.setAttribute("onclick", `document.getElementById('Xdisplay').contentWindow.location.replace("${par}/${ele}"); updateExplorer("${par}/${ele}")`);
+                par = `${par}/${ele.replace(".html", "")}`
+
+                let obj = Array(window.parent.document.getElementsByClassName("tools"))[0]
+                for (let key of Object.keys(obj)){
+                    obj[key].style.display = "";
+                }
+
+            }
+            let textnode = window.parent.document.createTextNode(ele.replace(".html", ""));
+            a.appendChild(textnode);
+            node.appendChild(a);
+            explorer.appendChild(node);
+        }
+    });
+}
---
+++ docs/Assets/js/tools/colab.js
@@ -0,0 +1,40 @@
+// https://raw.githubusercontent.com/googlecolab/open_in_colab/master/js/background.js
+
+/*
+    Copyright 2018 Google LLC
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+        https://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+*/
+
+// This listener is called when the user clicks the extension icon.
+
+// If the current URL matches a Jupyter notebook hosted on github.com
+// or on gist.github.com, this function will open a new tab and load
+// the notebook into Colab.
+
+// edited by XinYaanZyoy (https://github.com/XinYaanZyoy)
+
+function onColab() {
+    const colab_url = 'https://colab.research.google.com/';
+
+    let repo = "github/$GH$/$REPO$/"
+    let root = "blob/master/notebooks/"
+    let pathpoints = Object.entries(document.getElementsByClassName('virtual'));
+    let pointnames = []
+    for(let ele of pathpoints){
+        pointnames.push(ele[1].innerText);
+    }
+    let notebook = pointnames.join("/");
+    let url = colab_url + repo + root + notebook + '.ipynb';
+    window.open(url, '_blank').focus();
+}
