e9f97d2576f01edd0e6a4e0a40e8a3e4226a9560
XinYaanZyoy <XinYaanZyoy@gmail.com>
2019-08-27 12:08:47 +0000

    fix #15

--- app/converter/repoClonnerAndFetcher.py
+++ app/converter/repoClonnerAndFetcher.py
@@ -2,11 +2,12 @@
 
 import os
 import sys
-import itertools
 from git import Repo
 
 from Xlib import ccc
+from Xlib import ignoreReader
 from Xlib import workspaceCleaner as wc
+
 
 def fstatus(plus, minus, lines):
     """
@@ -30,9 +31,15 @@ def fstatus(plus, minus, lines):
     else:
         ccc.fail("while parsing file status code")
 
+
 class ClonnerAndFetcher:
+    """
+    clone, fetch, filter, and return
+    """
     def __init__(self, url):
         self.url = url
+        self.repo = None
+        self.author = None
 
     def clone(self):
         """
@@ -50,37 +57,41 @@ class ClonnerAndFetcher:
             sys.exit(ccc.stderr(err))
 
     def fetch(self):
+        """
+        fetches repo
+        """
         try:
-            toDelete = []
-            toRename = []
-            toConvert = []
+            ignore_list = ignoreReader.read()
+            to_delete = []
+            to_rename = []
+            to_convert = []
             ccc.white("Fetching", str(self.repo.head.commit)[:7])
             for fn, fd in list(self.repo.head.commit.stats.files.items()):
-                if fn.startswith("notebooks") and fn not in [] and "checkpoint" not in fn and ".ipynb" in fn:
+                if fn.startswith("notebooks") and fn not in ignore_list and "checkpoint" not in fn and ".ipynb" in fn:
                     status = fstatus(fd['insertions'], fd['deletions'], fd['lines'])
                     ccc.note("fetched " + fn + " with status " + status)
                     if status != "110" and status != "100" and status != "010":
                         if os.path.exists(os.path.join("Xblog", fn)) and fn.endswith(".ipynb"):
                             if status == "111" or status == "101" or status == "011":
-                                toConvert.append(fn)
+                                to_convert.append(fn)
                         else:
                             if status == "011" and fn.endswith(".ipynb"):
-                                toDelete.append(fn)
+                                to_delete.append(fn)
                         if status == "000":
                             src = os.path.join(fn.split("{")[0], fn.split("{")[1].split(" => ")[0])
                             des = os.path.join(fn.split("{")[0], fn.split("{")[1].split(" => ")[1].split("}")[0])
                             # if src.endswith(".ipynb") and des.endswith(".ipynb"):
-                            toRename.append((src, des))
+                            to_rename.append((src, des))
                     else:
                         ccc.fail("while decoding status code")
                         wc.cleanXblog()
                         sys.exit()
             ccc.success("fetching " + str(self.repo.head.commit)[:7])
             return {
-                    "to_be_converted": toConvert,
-                    "to_be_deleted": toDelete,
-                    "to_be_renamed": list(set(toRename))
-                    }
+                "to_be_converted": to_convert,
+                "to_be_deleted": to_delete,
+                "to_be_renamed": list(set(to_rename))
+                }
         except Exception as err:
             ccc.fail("while fetching latest commit changes")
             wc.cleanXblog()
