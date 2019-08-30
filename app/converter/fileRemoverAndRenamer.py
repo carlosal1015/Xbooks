cc1a318ace36ef363256a6bdeae8ada2e4c5e15f
XinYaanZyoy <XinYaanZyoy@gmail.com>
2019-08-30 14:40:23 +0000

    feat: automatically enebles README.md file as homepage if notebooks/welcome.ipynb doesn't exist, fixes #57

--- app/converter/fileRemoverAndRenamer.py
+++ app/converter/fileRemoverAndRenamer.py
@@ -8,6 +8,9 @@ from Xlib import closer
 from indexer import indexUpdater as IUp
 from indexer import indexUninstaller as IUn
 
+from . import md2html
+
+
 def rename(tbr, tipe):
     """
     renames Xpage or Xbook and returns True if success; False otherwise
@@ -22,6 +25,13 @@ def rename(tbr, tipe):
         des_pdf = tbr[1].replace(".ipynb", ".pdf").replace("notebooks", "pdfs")
         os.renames("Xblog/docs/" + src_pdf, "Xblog/docs/" + des_pdf)
         ccc.success("renaming " + src_pdf + " to " + des_pdf)
+        if tbr[0] == "notebooks/welcome.ipynb":
+            if os.path.isfile("Xblog/README.md"):
+                md2html.convert()
+            else:
+                with open("Xblog/docs/notebooks/welcomme.html", 'w') as f:
+                    f.write("<html>\n<body>\n<h1 align=\"center\">Welcome to Xbooks blogs!</h1>\n<h4 align=\"center\">This blog has no welcome page<br/>if you're maintainer of this blog, kindly write either README.md or notebooks/welcome.ipynb file!</h4>\n</body>\n</html>\n")
+                    f.close()
         return True
     except Exception as err:
         closer.close(err=err, fail="while renaming " + tbr[0] + " to " + tbr[1])
@@ -41,6 +51,13 @@ def delete(tbd, tipe):
             des_pdf = des.replace(".html",".pdf").replace("notebooks", "pdfs")
             os.remove(des_pdf)
             ccc.success("deleting " + des_pdf)
+            if tbd == "notebooks/welcome.ipynb":
+                if os.path.isfile("Xblog/README.md"):
+                    md2html.convert()
+                else:
+                    with open("Xblog/docs/notebooks/welcomme.html", 'w') as f:
+                        f.write("<html>\n<body>\n<h1 align=\"center\">Welcome to Xbooks blogs!</h1>\n<h4 align=\"center\">This blog has no welcome page<br/>if you're maintainer of this blog, kindly write either README.md or notebooks/welcome.ipynb file!</h4>\n</body>\n</html>\n")
+                        f.close()
         if tipe == "Xbook":
             shutil.rmtree(des)
             ccc.success("deleting " + des)
